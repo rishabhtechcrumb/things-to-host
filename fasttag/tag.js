68c9d215ee8b26a8ff7e198caa1f30b7391ae2c6
Rishabh Singh <126225564+rishabhtechcrumb@users.noreply.github.com>
2025-04-07 15:50:13 +0530

    Update tag.js

--- fasttag/tag.js
+++ fasttag/tag.js
@@ -1,7 +1,9 @@
-  function loadScriptTag(src, onload = () => { }, onError = () => { }) {
-    const srciptTag = document.createElement('script')
-    srciptTag.src = src;
-    srciptTag.onload = onload
-    document.head.appendChild(srciptTag)
+function loadScriptTag(src, onload = () => { }, onError = () => { }) {
+  const srciptTag = document.createElement('script')
+  srciptTag.src = src;
+  srciptTag.onload = onload
+  document.head.appendChild(srciptTag)
 }
+loadScriptTag("https://imasdk.googleapis.com/js/sdkloader/ima3.js")
 loadScriptTag("https://cdn.jsdelivr.net/gh/rishabhtechcrumb/things-to-host@refs/heads/master/fastIma/player.js")
+
