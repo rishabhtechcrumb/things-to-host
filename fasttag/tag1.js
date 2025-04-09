b25eaf5b8cc558adc6ee08a2f4153dd53e56b89e
Rishabh <rishabh@techcrumb.com>
2025-04-09 11:25:24 +0530

    dd

---
+++ fasttag/tag1.js
@@ -0,0 +1,16 @@
+function loadScriptTag(src, onload = () => { }, onError = () => { }) {
+  const srciptTag = document.createElement('script')
+  srciptTag.src = src;
+  srciptTag.onload = onload
+  document.head.appendChild(srciptTag)
+}
+
+const tagStartTime = performance.now();
+window.TC_PERF = window.TC_PERF ?? {};
+window.TC_PERF.tagStartTime = tagStartTime;
+
+loadScriptTag("https://imasdk.googleapis.com/js/sdkloader/ima3.js")
+loadScriptTag("https://cdn.jsdelivr.net/gh/rishabhtechcrumb/things-to-host@master/fastIma/player1.js")
+
+console.log(`TC_PERF:: ${performance.now() - window.TC_PERF.tagStartTime} tagloaded`);
+
