221b0e0d1dc34c05fc6ead8866ae912b6b1ec82d
Rishabh <rishabh@techcrumb.com>
2025-04-09 11:50:37 +0530

    dd

---
+++ reactfastima/tag3.js
@@ -0,0 +1,20 @@
+function loadScriptTag(src, onload = () => { }, onError = () => { }) {
+    const srciptTag = document.createElement('script')
+    srciptTag.src = src;
+    srciptTag.onload = onload
+    document.head.appendChild(srciptTag)
+}
+const tagStartTime = performance.now();
+window.TC_PERF = window.TC_PERF ?? {};
+window.TC_PERF.tagStartTime = tagStartTime;
+
+const adslotDiv = document.createElement('div')
+adslotDiv.id = 'root'
+document.body.insertBefore(adslotDiv, document.body.firstChild);
+
+loadScriptTag("https://imasdk.googleapis.com/js/sdkloader/ima3.js")
+loadScriptTag("https://cdn.jsdelivr.net/gh/rishabhtechcrumb/things-to-host@master/reactfastima/player2.js", () => {
+    TC_YAAS.setup("root");
+})
+
+console.log(`TC_PERF:: ${performance.now() - window.TC_PERF.tagStartTime} tagloaded`);
